8e25458aba54440ee7a59934b411f1bfb0355cf9
muondu <munenemuondu@gmail.com>
2019-12-16 13:04:42 +0300

    I hav made a small shop and I have created a readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# My Program
+If you ran this site the computer will be your shopkeeper and you will be the customer
+## Greeting
+It will start by saying "Hello,what is your name" and you enter your name and it will say hello and then the name that was been writen
+## Items
+The computer(Shopkeeer) will show you the list of items and you can choise any of the three items.
+## Bill
+The shopkeeper then will add and give you the bill.Then you write the amount you want pay.
+## Change
+And it will give you the correct Change
--- ts1.py
+++ ts1.py
@@ -1,10 +1,11 @@
-a = input("Hello what is your name:  ")
-
-b = print("Hello " + a)
-
-c = print("I will be your shopkeeper.This is the items.The item will be in dolars")
-
-t = {
+#Ask your name
+greeting = input("Hello what is your name:  ")
+#Print your name with the respond
+answer = print("Hello " + greeting)
+#The currency
+shopkeeper = print("I will be your shopkeeper.This is the items.The item will be in dolars")
+#The set which contains the list of items
+listofitems = {
                             
  "Milk" : 55,
 
@@ -20,26 +21,34 @@ t = {
 
 "Soda" : 50
 }
-print(t)
-k = t[input("What is your first item: ")]
-print(k)
+print(listofitems)
+#Asks you your first item
+item1 = listofitems[input("What is your first item: ")]
+print(item1)
+#Asks you your second item
+item2 = listofitems[input("What is your second item: ")]
+print(item2)
+#Asks you your third item
+item3 = listofitems[input("What is your third  item:  ")]
+print(item3)
+#The total of the items
+total = item1 + item2 + item3 
 
-l = t[input("What is your second item: ")]
-print(l)
-
-
-m = t[input("What is your third  item:  ")]
-print(m)
-
-n = k + l + m 
-
-o = str(n)
-print("Your total is " + o)
-
-p = input("Enter the amount you want to pay:  ")
+newTotal = int(total)
 
 
-q = o - n
+#The bill
+print("Your total is " + str(total))
+
+
+
+
+
+
+#What you want to pay
+amount = input("Enter the amount you want to pay:  ")
+
+change = int(amount) - newTotal
  
-
-print("Your change is " + q) 
+#Your change
+print("change you'll recieve " + str(change))
